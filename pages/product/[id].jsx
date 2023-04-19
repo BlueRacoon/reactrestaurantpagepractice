8b14d0cbf0ae1ed9f02cec1ed1b2f24e57702d75
Blue Raccoon <zachary_owen@aol.com>
2023-04-19 13:16:54 -0500

    adding remote.

---
+++ pages/product/[id].jsx
@@ -0,0 +1,89 @@
+import styles from "../../styles/Product.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+const Product = () => {
+  const [size, setSize] = useState(0);
+  const pizza = {
+    id: 1,
+    img: "/img/pizza.png",
+    name: "CAMPAGNOLA",
+    price: [19.99, 23.99, 27.99],
+    desc: "Lorem ipsum dolor sit amet, conse fiewnrf ,fwef. weinfowefm. There is so much pizza on this pizza oh my pizza what a pizza this is for pizza lovers that love to eat pizza. Although, it is still good for those that do not love pizza, so it really is a pizza for everyone.",
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.imgContainer}>
+          <Image src={pizza.img} objectFit="contain" alt="" layout="fill" />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <h1 className={styles.title}>{pizza.name}</h1>
+        <span className={styles.price}>${pizza.price[size]}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+        <h3 className={styles.choose}>Choose the size</h3>
+        <div className={styles.sizes}>
+          <div className={styles.size} onClick={() => setSize(0)}>
+            <Image src="/img/size.png" alt="" layout="fill" />
+            <span className={styles.number}>Small</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(1)}>
+            <Image src="/img/size.png" alt="" layout="fill" />
+            <span className={styles.number}>Medium</span>
+          </div>
+          <div className={styles.size} onClick={() => setSize(2)}>
+            <Image src="/img/size.png" alt="" layout="fill" />
+            <span className={styles.number}>Large</span>
+          </div>
+        </div>
+        <h3 className={styles.choose}>Choose additional ingredients</h3>
+        <div className={styles.ingredients}>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              id="double"
+              name="double"
+              className={styles.checkbox}
+            />
+            <label htmlFor="double">Double Ingredients</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              id="cheese"
+              name="cheese"
+              className={styles.checkbox}
+            />
+            <label htmlFor="cheese">Extra Cheese</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              id="spicy"
+              name="spicy"
+              className={styles.checkbox}
+            />
+            <label htmlFor="spicy">Spicy Sauce</label>
+          </div>
+          <div className={styles.option}>
+            <input
+              type="checkbox"
+              id="garlic"
+              name="garlic"
+              className={styles.checkbox}
+            />
+            <label htmlFor="galic">Garlic Sauce</label>
+          </div>
+        </div>
+        <div className={styles.add}>
+          <input type="number" defaultValue={1} className={styles.quantity} />
+          <button className={styles.button}>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
